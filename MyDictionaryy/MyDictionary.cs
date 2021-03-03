1777a0584df7e1dfc7789ad1c9ccd0a7bc6dcc01
hatcec <37028859+hatcec@users.noreply.github.com>
2021-03-03 16:46:33 +0300

    Proje dosyası ekle.

---
+++ MyDictionaryy/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryy
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _key;
+        TValue[] _value;
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+            _value = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] _tempKey = _key;
+            TValue[] _tempValue = _value;
+            _key = new TKey[_key.Length + 1];
+            _value = new TValue[_value.Length + 1];
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _key[i] = _tempKey[i];
+            }
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+            }
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+            Console.WriteLine(key + " " + value + " eklendi.");
+        }
+    }
+}
---
+++ MyDictionaryy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionaryy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("hatice", 25);
+            myDictionary.Add("Ali", 39);
+            myDictionary.Add("Musa", 98);
+            myDictionary.Add("Merve", 78);
+        }
+    }
+}
